f65ad0215e09c8ae704db327104a45b0271e21b7
stachecoding2 <stachecoding2@DESKTOP-0BN2DDD.localdomain>
2022-08-29 08:37:35 -0400

    backend and posts complete

--- twitter_app/app.js
+++ twitter_app/app.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const User = require('./models/User');
+const bodyParser = require('body-parser');
+
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to mongoDB"))
+    .catch(err => console.log(err))
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
+
+    console.log(res);
     res.send("Hello World");
 });
 
-const port = process.env.PORT || 5000;
+app.use("/api/users", users);
+app.use("/api/tweets", tweets)
+
+const port = process.env.PORT || 5001;
 
 app.listen(port, () => {console.log(`Listening on port ${port}`)});
